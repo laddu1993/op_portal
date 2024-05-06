11a6eb06f0b8e7721290e2420a95864a9c8ceb4c
Vinil Lakkavatri <vinil.lakkavatri@speridian.com>
2024-05-06 16:49:07 +0530

    css to sass file

--- Design/OrderPortal/src/app/component/customer-info/customer-info.component.ts
+++ Design/OrderPortal/src/app/component/customer-info/customer-info.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-customer-info',
   templateUrl: './customer-info.component.html',
-  styleUrls: ['./customer-info.component.css']
+  styleUrls: ['./customer-info.component.scss']
 })
 export class CustomerInfoComponent {
 
--- Design/OrderPortal/src/app/component/dealer-info/dealer-info.component.ts
+++ Design/OrderPortal/src/app/component/dealer-info/dealer-info.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dealer-info',
   templateUrl: './dealer-info.component.html',
-  styleUrls: ['./dealer-info.component.css']
+  styleUrls: ['./dealer-info.component.scss']
 })
 export class DealerInfoComponent {
 
