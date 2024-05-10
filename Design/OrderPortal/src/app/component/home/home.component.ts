2cade73efffa7524a1c9e3910f53323588ec02ca
Ajeesh-lal <112160284+Ajeesh-lal@users.noreply.github.com>
2024-05-10 07:49:40 +0530

    changes 4-10-24

--- Design/OrderPortal/src/app/component/home/home.component.ts
+++ Design/OrderPortal/src/app/component/home/home.component.ts
@@ -4,7 +4,7 @@ import {MatTabsModule} from '@angular/material/tabs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
 
